4f5bbef1224124a8131bcf60232cae312d2e87d9
codwiz23 <abdirahman.mohamed@maine.edu>
2025-10-24 15:31:57 -0400

    completed first 5kyu problem!

---
+++ 5kyu/SimplePigLatin.js
@@ -0,0 +1,26 @@
+/**
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+ * Examples
+ * pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+ * pigIt('Hello world !');     // elloHay orldway !
+ */
+
+function pigIt(str) {
+    //Code here
+    let letter = []
+    let final = []
+    let arr = str.split(' ').map(word => {
+        if (word === '!' || word === '.') {
+            return word;
+        } else {
+            let char = word.split('')
+            letter.push(char[0])
+            char.shift()
+            char.push(`${letter[0]}ay`)
+            letter.shift()
+            final.push(char.join(''))
+        }
+    }).join(' ')
+    let result = final.toString().split(',').join(' ')
+    return result;
+}
